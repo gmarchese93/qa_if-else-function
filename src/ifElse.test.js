b69aa8ab8b3920ff56767e7afdd5078320f77a63
Gianluca Marchese <gianluca.marchese93@gmail.com>
2026-02-03 10:02:01 +0000

    Add tests for ifElse

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,56 @@
 'use strict';
 
-describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+const ifElse = require('./ifElse');
 
-  it('should ', () => {
+test('calls condition callback', () => {
+  const condition = jest.fn(() => true);
+  const first = jest.fn();
+  const second = jest.fn();
 
-  });
+  ifElse(condition, first, second);
 
-  // write tests here
+  expect(condition).toHaveBeenCalled();
+});
+
+test('calls first callback when condition returns true', () => {
+  const condition = jest.fn(() => true);
+  const first = jest.fn();
+  const second = jest.fn();
+
+  ifElse(condition, first, second);
+
+  expect(first).toHaveBeenCalled();
+  expect(second).not.toHaveBeenCalled();
+});
+
+test('calls second callback when condition returns false', () => {
+  const condition = jest.fn(() => false);
+  const first = jest.fn();
+  const second = jest.fn();
+
+  ifElse(condition, first, second);
+
+  expect(second).toHaveBeenCalled();
+  expect(first).not.toHaveBeenCalled();
+});
+
+test('callbacks are called without arguments', () => {
+  const condition = jest.fn(() => true);
+  const first = jest.fn();
+  const second = jest.fn();
+
+  ifElse(condition, first, second);
+
+  expect(first).toHaveBeenCalledWith();
+  expect(condition).toHaveBeenCalledWith();
+});
+
+test('returns nothing', () => {
+  const condition = jest.fn(() => false);
+  const first = jest.fn();
+  const second = jest.fn();
+
+  const result = ifElse(condition, first, second);
+
+  expect(result).toBeUndefined();
 });
